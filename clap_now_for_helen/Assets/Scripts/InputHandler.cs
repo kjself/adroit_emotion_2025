cca334f11345add1ef54d601bdb1853a1307cc72
Athlie54 <kjself04@gmail.com>
2025-04-12 17:09:45 -0500

    input handler complete

--- clap_now_for_helen/Assets/Scripts/InputHandler.cs
+++ clap_now_for_helen/Assets/Scripts/InputHandler.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    private CueType cue;
+    //private Dictionary<string, CueType> cueDict;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,34 @@ public class InputHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.anyKey)
+        {
+            if(Input.GetKeyDown("e"))
+            {
+                cue = CueType.CLAP;
+            }
+            if(Input.GetKeyDown("r"))
+            {
+                cue = CueType.LAUGH;
+            }
+            if (Input.GetKeyDown("t"))
+            {
+                cue = CueType.IMPRESSED;
+            }
+            if (Input.GetKeyDown("c"))
+            {
+                cue = CueType.GASP;
+            }
+            if (Input.GetKeyDown("v"))
+            {
+                cue = CueType.SCREAM;
+            }
+        }
+
     }
+    public CueType getCue()
+    {
+        return cue;
+    }
+
 }
